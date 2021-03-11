2a9aca5c05ee8e6df1ddeb7363ae6cd5fd8c0ee6
SunSayIsCool <49588976+SunSayIsCool@users.noreply.github.com>
2021-03-11 13:18:03 +0200

    v 1.1 Addded MIL-STD 2525 points Added aliases for SA

--- src/SaToCoT/Main.java
+++ src/SaToCoT/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Harris SA/MotoTRBO to Cot Forwarder");
+        primaryStage.setTitle("Harris SA/MotoTRBO to CoT Forwarder");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.setResizable(false);
         primaryStage.show();
